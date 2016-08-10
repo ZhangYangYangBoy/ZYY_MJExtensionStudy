7869a24ec68fc17ade8e46fc03aff7f37ceda248
张洋洋 <yangyangzhang17@creditease.cn>
2016-08-10 14:27:03 +0800

    Initial Commit

---
+++ ZYY_MJExtensionStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYY_MJExtensionStudy
+//
+//  Created by Tech-zhangyangyang on 16/8/10.
+//  Copyright © 2016年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYY_MJExtensionStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYY_MJExtensionStudy
+//
+//  Created by Tech-zhangyangyang on 16/8/10.
+//  Copyright © 2016年 Tech-zhangyangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
